417c7c3ed747cbc29c2ef591164e303231637ada
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-06-06 14:47:18 +0800

    add sso sign provider config

--- src/main/kotlin/ProviderConfig.kt
+++ src/main/kotlin/ProviderConfig.kt
@@ -10,6 +10,11 @@ object ProviderConfig : ReadOnlyPluginConfig("config") {
     @ValueDescription("API链接，url = \"\$apiUrl/energy?name=...\"")
     val apiUrl by value("http://api.tencentola.com/txapi")
     @ValueName("api-version")
-    @ValueDescription("API算法版本，即参数 name 的值")
+    @ValueDescription("(算法)API算法版本，即参数 name 的值")
     val apiVersion by value("8.9.50")
+    @ValueName("api-version")
+    @ValueDescription("(签名)API算法版本，即参数 name 的值")
+    val apiVersionSign by value("8.9.58")
+    @ValueDescription("QUA，签名所需参数，可以在客户端内置浏览器的UA中获取，不知道怎么填的不要乱填")
+    val qua by value(mapOf("ANDROID_PHONE" to "V1_AND_SQ_8.9.50_3898_YYB_D"))
 }
