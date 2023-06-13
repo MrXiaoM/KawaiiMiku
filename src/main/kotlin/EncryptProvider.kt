842a40c72305c22cda1d44d6b8353acd7c853107
MrXiaoM <coolxiaom95@gmail.com>
2023-06-13 17:21:35 +0800

    add tlv544 encode method

--- src/main/kotlin/EncryptProvider.kt
+++ src/main/kotlin/EncryptProvider.kt
@@ -2,6 +2,7 @@ package top.mrxiaom.mirai.kawaii
 
 import net.mamoe.mirai.internal.spi.EncryptService
 import net.mamoe.mirai.internal.spi.EncryptServiceContext
+import top.dsbbs2.t544.Tlv544Sign
 
 object EncryptProvider : EncryptService {
     override fun encryptTlv(
@@ -9,6 +10,14 @@ object EncryptProvider : EncryptService {
         tlvType: Int,
         payload: ByteArray
     ): ByteArray? {
+        if (tlvType != 0x544) return null
+        val command = context.extraArgs[EncryptServiceContext.KEY_COMMAND_STR]
 
+        KawaiiMiku.logger.info("t544 command: $command")
+
+        val bytes = if (payload.last().toInt() == 0)
+            payload.copyInto(ByteArray(payload.size), 4, 4)
+        else payload
+        return Tlv544Sign.signBytes(bytes)
     }
 }
