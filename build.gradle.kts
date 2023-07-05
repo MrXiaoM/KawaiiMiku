90fc0beec11faf66a90110602dd091358a826d45
MrXiaoM <coolxiaom95@gmail.com>
2023-07-05 21:39:50 +0800

    add BuildConstants

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,31 +4,38 @@ plugins {
     kotlin("plugin.serialization") version kotlinVersion
 
     id("net.mamoe.mirai-console") version "2.15.0-core-pkgsso-19"
+    id("com.github.gmazzo.buildconfig") version "3.1.0"
 }
-allprojects {
-    group = "top.mrxiaom.mirai"
-    version = "0.1.2"
 
-    apply(plugin = "java")
-    java {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    repositories {
-        mavenLocal()
-        maven("https://repo.huaweicloud.com/repository/maven/")
-        mavenCentral()
-        maven("https://repo.mirai.mamoe.net/snapshots/")
-    }
-    tasks {
-        processResources {
-            from("LICENSE")
-            if (project != rootProject) {
-                rename("LICENSE", "LICENSE-" + project.name)
-            }
-        }
+group = "top.mrxiaom.mirai"
+version = "0.1.2"
+
+buildConfig {
+    className("BuildConstants")
+    packageName("top.mrxiaom.mirai.kawaii")
+    useKotlinOutput()
+
+    buildConfigField("String", "VERSION", "\"${project.version}\"")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenLocal()
+    maven("https://repo.huaweicloud.com/repository/maven/")
+    mavenCentral()
+    maven("https://repo.mirai.mamoe.net/snapshots/")
+}
+
+tasks {
+    processResources {
+        from("LICENSE")
     }
 }
+
 dependencies {
     implementation(platform("net.mamoe:mirai-bom:2.15.0-core-pkgsso-19"))
     compileOnly("net.mamoe:mirai-core")
--- src/main/kotlin/KawaiiMiku.kt
+++ src/main/kotlin/KawaiiMiku.kt
@@ -3,14 +3,13 @@ package top.mrxiaom.mirai.kawaii
 import net.mamoe.mirai.console.extension.PluginComponentStorage
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
-import net.mamoe.mirai.utils.info
 import kotlin.reflect.jvm.jvmName
 
 object KawaiiMiku : KotlinPlugin(
     JvmPluginDescription(
         id = "top.mrxiaom.mirai.kawaii",
         name = "KawaiiMiku",
-        version = "0.1.0",
+        version = BuildConstants.VERSION,
     ) {
         author("MrXiaoM")
     }
