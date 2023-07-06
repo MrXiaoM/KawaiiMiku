9960dfc63119de3806d361395fbe3da2f17847cf
MrXiaoM <coolxiaom95@gmail.com>
2023-07-06 13:08:27 +0800

    show the trust warning

--- src/main/kotlin/KawaiiMiku.kt
+++ src/main/kotlin/KawaiiMiku.kt
@@ -3,6 +3,7 @@ package top.mrxiaom.mirai.kawaii
 import net.mamoe.mirai.console.extension.PluginComponentStorage
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.internal.spi.EncryptService
 
 object KawaiiMiku : KotlinPlugin(
     JvmPluginDescription(
@@ -17,6 +18,11 @@ object KawaiiMiku : KotlinPlugin(
         ServiceConfig.reload()
         EncryptProvider.Factory.put(ServiceConfig.serviceUrl, ServiceConfig.serviceKey)
         EncryptProvider.Factory.registerAsOverride()
+
+        logger.warning("若使用本插件，即代表你信任本插件获取 qmiei、guid 等信息用于调用签名服务")
+        logger.warning("警告为信息安全相关的警告，如果你信任本插件，可忽略该警告")
+        @Suppress("INVISIBLE_MEMBER")
+        EncryptService.factory
 
         logger.info("Registered service: ${EncryptProvider.Factory::class.qualifiedName}")
     }
